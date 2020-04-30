d14dd890757e2ce4eaa5909b2e8566f44bca0f9b
Alejandro <aljand321@gmail.com>
2020-04-30 18:01:04 -0400

    formulario de portada ya envia imagenes solo falta que envie videos

--- src/components/portada/post.vue
+++ src/components/portada/post.vue
@@ -2,39 +2,36 @@
     <div class="mt-5">
         <b-row>
             <b-col cols="12" md="6" lg="6">
-                <b-form>
+                <b-form  @submit="post_metod">
                     <b-form-group
                         id="input-group-1"
-                        label="Email address:"
-                        label-for="input-1"
-                        
+                        label="Title"
+                        label-for="input-1"                        
                     >
                         <b-form-input
-                        id="input-1"
-                        
-                        type="text"
-                        required
-                        placeholder="Enter email"
+                            id="input-1"
+                            v-model="form_data.title123"
+                            type="text"
+                            required
+                            placeholder="Inserte un titutlo"
                         ></b-form-input>
-                    </b-form-group>
-                    <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
-                        <b-form-input
-                        id="input-2"
-                        
-                        required
-                        placeholder="Enter name"
-                        ></b-form-input>
-                    </b-form-group>
-                    <b-form-group id="textarea" label="Your Name:" label-for="textarea">
+                    </b-form-group>                    
+                    <b-form-group id="textarea" label="Descripcion" label-for="textarea">
                        <b-form-textarea
                         id="textarea"
-                       
-                        placeholder="Enter something..."
+                        v-model="form_data.description"
+                        placeholder="Inserte una descripcion del video...."
                         rows="3"
                         max-rows="6"
                         ></b-form-textarea>
                     </b-form-group>
-                    <b-button type="submit" variant="primary">Submit</b-button>
+                    <b-form-file
+                    @change="ver"
+                    placeholder="Choose a file or drop it here..."
+                    drop-placeholder="Drop file here..."
+                    ></b-form-file>
+                    <div class="mt-3">Selected file: {{ data_image ? data_image.name : '' }}</div>
+                    <b-button type="submit"  variant="primary">Submit</b-button>
                     
                     <b-button type="reset" variant="danger">Reset</b-button>                    
                 </b-form>
@@ -43,30 +40,91 @@
                <b-container fluid class="p-1 bg-dark">
                     <b-row>
                         <b-col>
-                            <div v-b-hover="hover_m">
-                                <div v-if="isHovered" class="py-3 px-4">
-                                    <b-img  thumbnail fluid src="https://thekpoprage.files.wordpress.com/2015/07/t-ara-so-crazy.jpg" alt="Image 1"></b-img>                    
+                            <div v-if="file == ''">
+                                <div  class="py-3 px-4">
+                                    <b-img  thumbnail fluid :src="noImg" alt="Image 1"></b-img>                    
                                 </div>
-                                 <div v-else class="py-4 px-5">
-                                    <b-img  thumbnail fluid src="https://thekpoprage.files.wordpress.com/2015/07/t-ara-so-crazy.jpg" alt="Image 1"></b-img>                    
+                                
+                            </div>
+                            <div  v-b-hover="hover_m" v-else-if="file.length > 0">
+                                <div v-if="isHovered" class="py-3 px-4">
+                                    <b-img  thumbnail fluid :src="file" alt="Image 1"></b-img>                    
+                                </div>
+                                <div v-else class="py-4 px-5">
+                                    <b-img  thumbnail fluid :src="file" alt="Image 1"></b-img>                    
                                 </div>
                             </div>                            
                         </b-col>
                     </b-row>
-                </b-container>                    
+                </b-container>    
             </b-col>
         </b-row>
+       
     </div>
 </template>
 <script>
+    import axios from 'axios';
+    var NO_image = require ('../../assets/noImage.png')
     export default {
         data:() => ({
-            isHovered: false
+            isHovered: false,
+            file:'',
+            data_image : '',
+            noImg:NO_image,
+            form_data:{
+                
+                title123:'',
+                description:''
+            }
         }),
         methods:{
             hover_m(data){
                 //console.log(data)
                 this.isHovered = data
+            },
+            async post_metod(e){
+                e.preventDefault();                
+                const params_data = new FormData();
+                params_data.append('title', this.form_data.title123)
+                params_data.append('description', this.form_data.description)
+                params_data.append('music', this.data_image)
+                try{
+                    var enviar = await this.axios.post('http://localhost:3000/portada',
+                    params_data,
+                    {
+                        headers: {
+                            'Content-Type': 'multipart/form-data'
+                        }
+                    })
+
+                    console.log(enviar, " esto es la respuesta que queiro ver ")
+                }catch (error){
+                    console.log(error)
+                }finally{
+                    console.log('finalizo')
+                }                
+                
+            },
+            async ver (event) { // esta funcion es para poder leer  una imagen y mostrarlo en pantalla
+               var img = event.target;
+                if (img.files && img.files[0]){
+
+                    this.data_image = img.files[0]
+
+                    var reader = new FileReader();
+                    reader.onload = (e) => {
+                        this.file =  e.target.result;
+                        
+                        console.log(img.files)
+                    }
+                    //reader.readAsDataURL(img.files[0])
+                    reader.readAsDataURL(this.data_image)
+                }
+               
+            },
+
+            esto(){
+                console.log(this.file)
             }
         }
     }
--- src/main.js
+++ src/main.js
@@ -4,11 +4,14 @@ import router from './router'
 import store from './store'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 // Install BootstrapVue
 Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
-
+Vue.use(VueAxios, axios)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
